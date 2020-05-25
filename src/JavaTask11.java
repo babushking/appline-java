d47c272ca3d150e6b506f3cf187329a892f099c4
Babosso <babos-87@mail.ru>
2020-05-25 20:06:14 +0300

    выполнил задание 11

---
+++ src/JavaTask11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class JavaTask11 {
+  public static void main(String[] args) {
+
+    Scanner text = new Scanner(System.in);
+    System.out.println("Введите текст");
+    String simbol = text.nextLine();
+    System.out.println(simbol);
+
+
+  }
+}
