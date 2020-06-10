fc51be97c3d6e5cfd5f1f7df85048a4b2b1df675
Babosso <babos-87@mail.ru>
2020-06-10 22:45:01 +0300

    задание 18

--- src/JavaTask18.java
+++ src/JavaTask18.java
@@ -4,31 +4,53 @@ class JavaTask18 {
   public static void main(String[] args) {
     String s;
 
-    try (BufferedReader br = new BufferedReader(new FileReader("D://Program Files//загрузки//задание 1.txt"))) {
-      while ((s = br.readLine()) != null) {
+    int counter = 0;
+    try {
+      BufferedReader br = new BufferedReader(new FileReader("D://Program Files//загрузки//задание 1.txt"));
+
+      while (true) {
+        s = br.readLine();
+
+        if (s == null) {
+          break;
+        }
+
+        counter++;
         System.out.println(s);
+
+
       }
-    } catch (IOException exc) {
+
+      System.out.println(counter + " строк прочитано");
+    }
+    catch (IOException exc) {
       System.out.println("Ошибка ввода-вывода: " + exc);
     }
 
 
-    String str;
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("Введите текст для записи в файл. Признак окончания ввода-строка 'stop'");
-    try (FileWriter fw = new FileWriter("D://Program Files//загрузки//задание 1.txt")) {
-      do {
-        System.out.print("^");
-        str = br.readLine();
+    if (counter >0){
+      String str;
+      Reader reader = new InputStreamReader(System.in);
+      BufferedReader br = new BufferedReader(reader);
+      System.out.println("Введите " + counter + " строк для записи в файл.");
+      try {
+        FileWriter fw = new FileWriter("D://Program Files//загрузки//задание 1.txt");
 
-        if (str.compareTo("stop") == 0) break;
-
-        str = str + "\r\n";
-        fw.write(str);
-
-      } while (str.compareTo("stop") != 0);
-    } catch (IOException exc) {
-      System.out.println("Ошибка ввода-вывода: " + exc);
+        do {
+          System.out.print("^");
+          str = br.readLine();
+          str = str + "\r\n";
+          fw.write(str);
+          counter--;
+        }
+        while (counter != 0);
+        fw.close();
+      } catch (IOException exc) {
+        System.out.println("Ошибка ввода-вывода: " + exc);
+      }
+    }
+    else {
+      System.out.println("Исходный файл пустой");
     }
   }
 }
